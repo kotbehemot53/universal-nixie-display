9dad9d6acd9e3cfc3506c01758f9850b5af66b87
Marek Wójcikowski <kot.behemot53@gmail.com>
2023-02-16 02:08:15 +0100

    commands final form?

--- firmware/vfd-controller/lib/IV18Display/IV18I2CCommandExecutor.h
+++ firmware/vfd-controller/lib/IV18Display/IV18I2CCommandExecutor.h
@@ -6,12 +6,11 @@
 
 class IV18Animator;
 
+// TODO: amend nixie commands appropriately (the way they work + names)
 class IV18I2CCommandExecutor
 {
 private:
     static const short BUNCHED_COMMANDS_BUFFER_LENGTH = 128;
-
-    // TODO: commands for fade-in and fade-out!
 
     // instant commands
     static const byte CMD_OFF = 0x10; //turn the lamps off
@@ -21,15 +20,20 @@ private:
     static const byte CMD_INTRO_ON = 0x12; //turn the intro on
 
     // bunched commands
-    static const byte CMD_FIN = 0x20; //loads number, commas and dimming values from buffers to output (renders new frame)
+    static const byte CMD_MULTI_FIN = 0x20; //loads number, commas and dimming values from buffers to output (renders new frame)
     // TODO: maybe set duty cycle by separate byte for more precision?
-    static const byte CMD_DIMMER = 0x60; //last 4 bits set the duty cycle; it's assigned to the latest digit set
+    //       DECISION: yes
+    static const byte CMD_MULTI_DIGIT_DIMMER = 0x60; // the FOLLOWING byte sets the duty cycle; last 4 bits determine which digit
     // TODO: do we want to set only chosen digits and retain old ones? or always set all 9? taking dimmer into account,
     //       maybe always set all or reset unset digits to space
-    static const byte CMD_CHAR = 0x80; // the FOLLOWING byte sets char; last 4 bits determine which digit
-    static const byte CMD_BYTE = 0x90; // the FOLLOWING byte sets raw segments byte; last 4 bits determine which digit
-    // TODO: do we want to reset commas every frame? all retain old commas and define a separate command to turn them off?
-    static const byte CMD_LAMP_POINT_R = 0xF0; //last 4 bits determine which right comma should be on
+    //       DECISION: reset unset ones to space (on FIN)
+    static const byte CMD_MULTI_DIGIT_CHAR = 0x80; // the FOLLOWING byte sets char; last 4 bits determine which digit
+    static const byte CMD_MULTI_DIGIT_BYTE = 0x90; // the FOLLOWING byte sets raw segments byte; last 4 bits determine which digit
+    // TODO: do we want to reset commas every frame? or retain old commas and define a separate command to turn them off?
+    //       DECISION: reset unset ones to no comma (on FIN)
+    static const byte CMD_MULTI_DIGIT_POINT_R = 0xF0; // last 4 bits determine which right comma should be on
+    static const byte CMD_MULTI_DIGIT_FADE_IN = 0xA0; // the FOLLOWING byte sets target duty cycle; last 4 bits determine which digit
+    static const byte CMD_MULTI_DIGIT_FADE_OUT = 0xB0; // the FOLLOWING byte sets target duty cycle; last 4 bits determine which digit
 
     static byte bunchedCommandsBuffer[BUNCHED_COMMANDS_BUFFER_LENGTH];
 
