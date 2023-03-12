19d1684def8dcf556afda60b1e325a6417f3dee5
Marek Wójcikowski <kot.behemot53@gmail.com>
2023-03-12 16:33:15 +0100

    idiotic error fix

--- software/raspberry/i2cnums/i2CComms.py
+++ software/raspberry/i2cnums/i2CComms.py
@@ -1,11 +1,11 @@
 from smbus import SMBus
 
 bus = SMBus(0) # indicates /dev/ic2-0
-def sendByte(addr, sendByte, cnt = 0):
+def sendByte(addr, byteToSend, cnt = 0):
     try:
         #print('sendin '+bin(send_byte))
-        bus.write_byte(addr, sendByte)
+        bus.write_byte(addr, byteToSend)
     except:
         print('snd fail no ' + str(cnt + 1))
         if (cnt < 10):
-            sendByte(addr, sendByte, cnt + 1)
+            sendByte(addr, byteToSend, cnt + 1)
