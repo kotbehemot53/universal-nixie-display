5226249a35ca8c946f2b92efa145da10fca54b84
Marek Wójcikowski <kot.behemot53@gmail.com>
2023-03-12 14:59:46 +0100

    who's the idiot

--- software/raspberry/i2cnums/piToVFD.py
+++ software/raspberry/i2cnums/piToVFD.py
@@ -5,3 +5,12 @@ bus = SMBus(0) # indicates /dev/ic2-0
 
 def sendOff():
     sendByte(0x10)
+
+def sendByte(send_byte, cnt = 0):
+    try:
+        #print('sendin '+bin(send_byte))
+        bus.write_byte(addr, send_byte)
+    except:
+        print('snd fail no ' + str(cnt + 1))
+        if (cnt < 10):
+            sendByte(send_byte, cnt + 1)
