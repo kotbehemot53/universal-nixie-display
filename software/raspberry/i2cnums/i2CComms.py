46b29408ed37aae3ba815b00e9a86838a4732827
Marek Wójcikowski <kot.behemot53@gmail.com>
2023-04-24 19:30:54 +0200

    intervals after i2c commands

--- software/raspberry/i2cnums/i2CComms.py
+++ software/raspberry/i2cnums/i2CComms.py
@@ -1,10 +1,12 @@
 from smbus import SMBus
+import time
 
 bus = SMBus(0) # indicates /dev/ic2-0
 def sendByte(addr, byteToSend, cnt = 0):
     try:
         #print('sendin '+bin(send_byte))
         bus.write_byte(addr, byteToSend)
+        time.sleep(0.01) # maybe this will ensure that the receiver can handle all messages
     except:
         print('snd fail no ' + str(cnt + 1))
         if (cnt < 10):
